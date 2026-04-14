fe65f265b915120d065cfadb765ff2cfd47af314
Pragadheeshwar D <pragadheesh06112007@gmail.com>
2026-04-14 11:01:21 +0530

    Easy - Remove Duplicates from Sorted Array

---
+++ Array/Easy/26-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int>a;
+        int k=1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]!=nums[i-1]){
+                nums[k++]=nums[i];
+            }
+        }
+        return k;
+    }
+};
