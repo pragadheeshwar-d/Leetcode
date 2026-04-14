0f196dbe9a22d199e172a15b2dcd6369db298333
Pragadheeshwar D <pragadheesh06112007@gmail.com>
2026-04-14 11:02:02 +0530

    Easy - Largest Triangle Area

---
+++ Array/Easy/812-largest-triangle-area.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    double largestTriangleArea(vector<vector<int>>& points) {
+        double maxArea = 0.0;
+        int n = points.size();
+        
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                for (int k = j + 1; k < n; ++k) {
+                    
+                    double x1 = points[i][0], y1 = points[i][1];
+                    double x2 = points[j][0], y2 = points[j][1];
+                    double x3 = points[k][0], y3 = points[k][1];
+                    
+                    double currentArea = 0.5 * abs(
+                        x1 * (y2 - y3) +
+                        x2 * (y3 - y1) +
+                        x3 * (y1 - y2)
+                    );
+                    
+                    maxArea = max(maxArea, currentArea);
+                }
+            }
+        }
+        return maxArea;
+    }
+};
