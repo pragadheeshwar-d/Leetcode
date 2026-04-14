c0b5aa9f6584f078ab5cf5e9defeae0a67d87af6
Pragadheeshwar D <pragadheesh06112007@gmail.com>
2026-04-14 11:00:34 +0530

    Easy - Palindrome Linked List

---
+++ Linked-List/Easy/234-palindrome-linked-list.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<int>s;
+        ListNode* i;
+        for(i = head;i!=NULL;i=i->next){
+            s.push(i->val);
+        }
+        while(head!=NULL){
+            if(s.top()!=head->val){
+                return false;
+            }
+            s.pop();
+            head=head->next;
+        }
+        return true;
+    }
+};
