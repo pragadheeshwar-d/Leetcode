bb2649734ed9f207b86a8cb84a48bc5073b93a4e
Pragadheeshwar D <pragadheesh06112007@gmail.com>
2026-04-14 11:01:13 +0530

    Easy - Best Time to Buy and Sell Stock

---
+++ Array/Easy/121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min = INT_MAX;
+        int profit = 0;
+        for(int i = 0; i < prices.size(); i++) {
+            if(prices[i] < min) {
+                min = prices[i];
+            }
+            int p = prices[i] - min;
+            if(p > profit) {
+                profit= p;
+            }
+        }
+        return profit;
+    }
+};
